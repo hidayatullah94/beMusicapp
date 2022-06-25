63af7ab66baefd3d7a9ecb30fa20e4d51ecd118f
hidayatullah94 <hidayatullah1780@gmail.com>
2022-06-26 00:02:57 +0700

    v1

---
+++ src/utils/song.js
@@ -0,0 +1,33 @@
+const songs = ({
+  id,
+  title,
+  performer,
+
+}) => ({
+  id,
+  title,
+  performer,
+
+});
+
+const detailSong = ({
+  id,
+  title,
+  year,
+  genre,
+  performer,
+  duration,
+  album_id,
+
+}) => ({
+  id,
+  title,
+  year,
+  genre,
+  performer,
+  duration,
+  albumId: album_id,
+
+});
+
+module.exports = { songs, detailSong };
